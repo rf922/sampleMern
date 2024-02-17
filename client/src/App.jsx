4faaec0aaf6ae2c0fb06fccf975d8ed3a39a9c70
CleveyP <clevelandplonsey@gmail.com>
2024-02-16 20:51:36 -0800

    initialize new react vite project in client folder. Install: cors, body-parser in server. Install axios in frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Login from './components/login/Login'
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+const App = () => {
+
+  return (
+    <div className="App">
+     <header>Hello this is a sample login form</header>
+     <Login />
+    </div>
+  )
+}
+
+export default App

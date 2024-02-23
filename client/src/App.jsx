61105ab4838d3a235c138615b874b4d18577cd52
Cleveland Plonsey <87806855+CleveyP@users.noreply.github.com>
2024-02-22 23:46:44 -0800

    8 setup react router dom (#9)
    * mysql connected. frontend uses react-router-dom install dotenv mysql2
    
    * install prettier in client and server. Install eslint in server. Create format and lint scripts. format all frontend and backend files.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import './App.css'
-import Login from './components/login/Login'
+import React from "react";
+import "./App.css";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 // import dotenv from 'dotenv';
 // dotenv.config();
 
 const App = () => {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/Register" element={<Register />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
-  return (
-    <div className="App">
-     <Login />
-    </div>
-  )
-}
-
-export default App
+export default App;
---
+++ client/src/components/register/algorithms.js
@@ -0,0 +1,8 @@
+const isValidForm = (username, password, confirmPassword) => {
+    if (username.length < 3 || password.length < 6 || password !== confirmPassword) {
+        return false;
+    }
+    return true;
+};
+
+export default isValidForm;

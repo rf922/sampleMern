ea5c54702f091ac48eb6e5b502cf4dcdfc538f63
CleveyP <clevelandplonsey@gmail.com>
2024-02-23 00:18:20 -0800

    fix: format the client files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import Login from "./components/login/Login";import Register from "./components/register/Register";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 // import dotenv from 'dotenv';
 // dotenv.config();

5900c1a1e154112dc10dc2f6c0d37d264d53c4b0
CleveyP <clevelandplonsey@gmail.com>
2024-02-23 00:02:47 -0800

    fixed formating error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/login/Login";
-import Register from "./components/register/Register";import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Register from "./components/register/Register";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 // import dotenv from 'dotenv';
 // dotenv.config();
 

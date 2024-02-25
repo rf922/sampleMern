d5904d0c5bcea96f004488b253efac234b8a72d5
CleveyP <clevelandplonsey@gmail.com>
2024-02-24 18:59:03 -0800

    install express-session, express-mysql-session setup session and session store in app.js

--- server/app.js
+++ server/app.js
@@ -3,17 +3,48 @@ const app = express();
 const port = process.env.PORT || 8080;
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
+//setup express session
+const sessionStore = new MySQLStore(
+  {},
+  require("./config/db")
+);
+//mount the session store on the app server
+app.use(
+  session({
+    secret: "session_cookie_secret",
+    store: sessionStore,
+    resave: false,  
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.PRODUCTION,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+      name: "sampleMernCookie",
+    },
+  }),
+);
+
+
+
+
 app.use("/user", require("./routers/userRouter")); //http://localhost:8080/user/login
 
 app.get("/", (req, res) => {
+  req.session.isLoggedIn = true;
+  console.log(JSON.stringify(req.session));
   res.send("Hello World!");
 });
+
+
+
+
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}`);
